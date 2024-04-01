5c61864609476a3ac16a90a6efb3ac31d207128f
Bruno Fernandes Campos <97699257+BrunoFerCam@users.noreply.github.com>
2024-04-01 08:45:17 -0300

    Envio inicial da estrutura do site (React e Css)
    Envio da base do site no estilo single-page, usando html e css.

--- sga/src/App.tsx
+++ sga/src/App.tsx
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Corpo'>
+       <nav>
+   <a href="#first"><i className='Pesquisa'>Pes</i></a>
+   <a href="#second"><i className='Relatorio'>Rel</i></a>
+   <a href="#third"><i className='Tabela'>Tab</i></a>
+   <a href="#fourth"><i className="Adicionar">Add</i></a>
+   <a href="#fifth"><i className="Editar">Edt</i></a>
+   <a href="#sixth"><i className="Notificacao">Not</i></a>
+   <a href="#seventh"><i className="Home">Hom</i></a>
+ </nav>
+  
+<div className= 'container'> 
+  <section id= 'first'>
+    <h1>Pesquisa</h1>
+  </section> 
+  
+  <section id= 'second'>
+    <h1>Relatório</h1>
+  </section>
+  
+ <section id= 'third'>
+   <h1>Tabela</h1>
+  </section>
+  
+ <section id= 'fourth'>
+   <h1>Adicionar</h1>
+  </section>
+
+  <section id= 'fifth'>
+   <h1>Editar</h1>
+  </section>
+
+  <section id= 'sixth'>
+   <h1>Notificação</h1>
+  </section>
+
+  <section id= 'seventh'>
+   <h1>Home</h1>
+  </section>
+</div>
     </div>
   );
 }
