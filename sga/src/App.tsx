c2d2a26f65ce33e4f9b60d0fa907a93c0594ee34
Bruno Fernandes Campos <97699257+BrunoFerCam@users.noreply.github.com>
2024-04-05 10:06:14 -0300

    Implementação do atalho da página de login
    Implementação inicial do sistema de login/perfil com mudanças em várias partes do css

--- sga/src/App.tsx
+++ sga/src/App.tsx
@@ -5,43 +5,42 @@ function App() {
   return (
     <div className='Corpo'>
        <nav>
-   <a href="#first"><i className='Pesquisa'><img className='icon' src='img/pesquisar.png'></img></i></a>
-   <a href="#second"><i className='Relatorio'><img className='icon' src='img/relatorio.png'></img></i></a>
-   <a href="#third"><i className='Tabela'><img className='icon' src='img/tabela.png'></img></i></a>
-   <a href="#fourth"><i className="Adicionar"><img className='icon' src='img/adicionar.png'></img></i></a>
-   <a href="#fifth"><i className="Editar"><img className='icon' src='img/editar.png'></img></i></a>
-   <a href="#sixth"><i className="Notificacao"><img className='icon' src='img/notif.png'></img></i></a>
-   <a href="#seventh"><i className="Home"><img className='icon' src='img/home.png'></img></i></a>
+   <a href="#Pesquisa"><i className='Pesquisa'><img className='icon' src='img/pesquisar.png'></img></i></a>
+   <a href="#Relatorio"><i className='Relatorio'><img className='icon' src='img/relatorio.png'></img></i></a>
+   <a href="#Tabela"><i className='Tabela'><img className='icon' src='img/tabela.png'></img></i></a>
+   <a href="#Adicionar"><i className="Adicionar"><img className='icon' src='img/adicionar.png'></img></i></a>
+   <a href="#Editar"><i className="Editar"><img className='icon' src='img/editar.png'></img></i></a>
+   <a href="#Notificacao"><i className="Notificacao"><img className='icon' src='img/notif.png'></img></i></a>
+   <a href="#Home"><i className="Home"><img className='icon' src='img/home.png'></img></i></a>
  </nav>
-  
+ <div className="navbar">
+  <a href="#Perfil"><i className='Perfil'><img className='icon' src='img/perfil.png'></img></i></a>
+</div>
 <div className= 'container'> 
-  <section id= 'first'>
+  <section id= 'Pesquisa'>
     <h1>Pesquisa</h1>
   </section> 
-  
-  <section id= 'second'>
+  <section id= 'Relatorio'>
     <h1>Relatório</h1>
   </section>
-  
- <section id= 'third'>
+ <section id= 'Tabela'>
    <h1>Tabela</h1>
   </section>
-  
- <section id= 'fourth'>
+ <section id= 'Adicionar'>
    <h1>Adicionar</h1>
   </section>
-
-  <section id= 'fifth'>
+  <section id= 'Editar'>
    <h1>Editar</h1>
   </section>
-
-  <section id= 'sixth'>
+  <section id= 'Notificacao'>
    <h1>Notificação</h1>
   </section>
-
-  <section id= 'seventh'>
+  <section id= 'Home'>
    <h1>Home</h1>
   </section>
+  <section id= 'Perfil'>
+    <h1>Perfil/Login</h1>
+  </section> 
 </div>
     </div>
   );
