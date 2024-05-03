40f7b9e329208a5a03f606a47d2d14539d312380
Bruno Fernandes Campos <97699257+BrunoFerCam@users.noreply.github.com>
2024-05-03 08:42:42 -0300

    Adição do css na pagina de Notificacao

--- sga/src/index.tsx
+++ sga/src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './componentes/BaseCSS/index.css';
 import reportWebVitals from './reportWebVitals';
 import Login from './componentes/Pages/Login/inicio';
+import Roteador from './componentes/Roteamento/roteador';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <Login/>
+      <Roteador/>
   </React.StrictMode>,
 
 );
