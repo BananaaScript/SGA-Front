b640d12d370eecc4b2a9317561078467e9bfd5a9
kaueriki <126582273+kaueriki@users.noreply.github.com>
2024-05-13 08:50:24 -0300

    puxei os estados para fazer o gráfico

--- sga/src/componentes/Pages/Relatorio/Componentes/Relatorio01.tsx
+++ sga/src/componentes/Pages/Relatorio/Componentes/Relatorio01.tsx
@@ -1,21 +1,81 @@
 import { useState } from "react"
+import {Ativo} from "../../../../modelos/ativo"
+import { useEffect } from "react"
+import axios from "axios"
+import {format} from 'date-fns'
 
-export default function Relatorio01(){
-    const [selecionado, setSelecionado] = useState('Nenhum')
-    const selecionarComponente=(escolhido: string) => {
-        setSelecionado(escolhido)
-    }
+export default function EditaAtivo(){
+    const[ativos, setAtivos] = useState<Array<Ativo>>([])
+    const [id, setId] = useState('')
+    const [nome, setNome]= useState('')
+    const [numeroAtivo, setNumeroAtivo] = useState('')
+    const [dataManutencao, setDataManutencao] = useState('')
+    const [estado, setEstadoAtivo] = useState('')
+    const [editando, setEditando] = useState(false)
+    const [filtro, setFiltro] = useState<string>('');
 
-    return(
+    useEffect(()=>{
+        axios.get('http://localhost:8080/ativo/listar')
+        .then((response)=>{
+            setAtivos(response.data)
+        })
+        .catch((error)=>{
+            console.error(error)
+        })
+    }, [])
+    const handleFiltroChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFiltro(event.target.value);
+    };
+
+    const ativosFiltrados = ativos.filter(ativo =>
+        ativo.nome.toLowerCase().includes(filtro.toLowerCase()) ||
+        ativo.rua.toLowerCase().includes(filtro.toLowerCase()) ||
+        ativo.bairro.toLowerCase().includes(filtro.toLowerCase()) ||
+        ativo.complemento.toLowerCase().includes(filtro.toLowerCase()) 
+    );
+
+     function formataData(data: string){
+        const dataFormatada = format(new Date(data), 'dd/MM/yyyy')
+        dataFormatada.toString()
+        return dataFormatada
+     }
+
+     return(
         <>
-            <div>
-            <div >
+        <div>
+            <div className="BoxTabela">
+                <h2>Ativos Cadastrados</h2>
+                    <input 
+                        type="text"
+                        value={filtro}
+                        onChange={handleFiltroChange}
+                        placeholder="Filtrar por Nome"
+                    />
+                <table>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Número ativo</th>
+                                <th>Estado do Ativo</th>
+                                <th>Data de Manutenção</th>
+                            </tr>
+                        </thead>
+                         <tbody>
+                            {ativosFiltrados.map((ativo)=>(
+                                <tr key={ativo.id}>
+                                    <td>{ativo.nome}</td>
+                                    <td>{ativo.numAtivo}</td>
+                                    <td>{ativo.estado}</td>
 
-<h2>Estamos Trabalhando Nisso</h2>
-<h4>Veja quando ficará pronto <a href="https://github.com/BananaaScript/SGA" target="_blank">AQUI</a></h4>
+                                    <td>{formataData(ativo.dataManutencao)}</td>
 
-</div>
+                                    
+                                </tr>
+                            ))}
+                        </tbody>
+                </table>
             </div>
+        </div>
         </>
     )
 }
